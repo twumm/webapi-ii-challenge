6b07d37a956dc3234a0974a6a0468d893a0393d5
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-09 12:17:51 +0000

    feature: Create all routes Return string of what each route should do Test all the routes

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const port = 5000
 
 server.use(express.json());
 
+server.post('/api/posts', async (req, res) => {
+  res.send('add blog post').end();
+})
+
+server.post('/api/posts/:id/comments', async (req, res) => {
+  res.send('add comments to post').end();
+})
+
+server.get('/api/posts', async (req, res) => {
+  res.send('get all posts').end();
+})
+
+server.get('/api/posts/:id', async (req, res) => {
+  res.send('returns post with specified id').end();
+})
+
+server.get('/api/posts/:id/comments', async (req, res) => {
+  res.send('returns an array of comment objects with the post with the specified id').end();
+})
+
+server.delete('/api/posts/:id', async (req, res) => {
+  res.send('removes the post with the specified id and returns the deleted post object').end()
+})
+
+server.put('/api/posts/:id', async (req, res) => {
+  res.send('updates post with specified id');
+})
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 })
