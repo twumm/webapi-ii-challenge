ff61ba054e4b5ce0c4a280012b41102a42146525
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-09 14:50:50 +0000

    chore: Install and require cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 
 const postRoutes = require('./blogs/post-routes');
@@ -6,6 +7,7 @@ const postRoutes = require('./blogs/post-routes');
 const port = 5000
 
 server.use(express.json());
+server.use(cors());
 server.use(postRoutes);
 
 server.listen(port, () => {
